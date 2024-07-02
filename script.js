b448dda696bb637c31816752c31b49c1fc92dbdf
Stephen Adeniji <praiseadeniji2017@gmail.com>
2024-07-02 08:41:26 +0100

    feat: update time

--- script.js
+++ script.js
@@ -1,37 +1,17 @@
 // To display both UTC and local time
-function updateTimeAndDay() {
-  const currentTimeUTCElement = document.getElementById("currentTimeUTC");
-  const currentTimeLocalElement = document.getElementById("currentTimeLocal");
-  const currentDayElement = document.getElementById("currentDay");
-
+function updateTime() {
   const now = new Date();
   const utcTime = now.toUTCString();
   const localTime = now.toLocaleTimeString();
-  const day = now.toLocaleDateString("en-US", { weekday: "long" });
+  const currentDay = now.toLocaleDateString("en-US", { weekday: "long" });
 
-  currentTimeUTCElement.textContent = utcTime;
-  currentTimeLocalElement.textContent = localTime;
-  currentDayElement.textContent = day;
+  document.getElementById("currentTimeUTC").textContent = utcTime;
+  document.getElementById("currentTimeLocal").textContent = localTime;
+  document.getElementById("currentDay").textContent = currentDay;
 }
 
-document.addEventListener("DOMContentLoaded", updateTimeAndDay);
+// display time
+updateTime();
 
-/* 
-
-UTC time only
-
-*/
-
-// function updateTimeAndDay() {
-//   const currentTimeElement = document.getElementById("currentTime");
-//   const currentDayElement = document.getElementById("currentDay");
-
-//   const now = new Date();
-//   const utcTime = now.toUTCString();
-//   const day = now.toLocaleDateString("en-US", { weekday: "long" });
-
-//   currentTimeElement.textContent = utcTime;
-//   currentDayElement.textContent = day;
-// }
-
-// document.addEventListener("DOMContentLoaded", updateTimeAndDay);
+// Update time every second
+setInterval(updateTime, 1000);
